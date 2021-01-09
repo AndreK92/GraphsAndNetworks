35ebfe43dae64e2857df636d4f2f8dee19348dfc
AndreK92 <56956080+AndreK92@users.noreply.github.com>
2021-01-09 12:12:26 +0100

    TopSort comments

--- TopSort.java
+++ TopSort.java
@@ -1,57 +1,57 @@
 import java.util.*;
 import java.io.*;
 
+
+// Topologische Sortierung mit einer AdjaListe
 public class TopSort {
-    ArrayList<ArrayList<Integer>> Graph;
-    int vertCount = 0;
+    //
+    ArrayList<ArrayList<Integer>> adjaListe;
+    int nodeCount = 0;
 
     public TopSort(ArrayList<ArrayList<Integer>> Graph)
     {
-        this.Graph = Graph;
-        this.vertCount = Graph.size()+1;
+        this.adjaListe = Graph;
+        this.nodeCount = Graph.size()+1;
     }
 
-    // A recursive function used by topologicalSort 
-    void topologicalSortUtil(int v, boolean visited[], Stack<Integer> stack) 
+    // Rekursive Funktion, die von topSot() verwendet wird
+    void topSortU(int v, boolean nodeVisited[], Stack<Integer> stack) 
     { 
-        // Mark the current node as visited. 
-        visited[v] = true; 
+
+        // Den aktuellen Knoten als überprüft markieren
+        nodeVisited[v] = true; 
+        // Aktueller Knoten
         Integer i; 
-  
-        // Recur for all the vertices adjacent 
-        // to thisvertex 
-        Iterator<Integer> it = Graph.get(v-1).iterator(); 
+
+        // Rekursion für alle Knoten Adjazent zu aktuellem Knoten
+        Iterator<Integer> it = adjaListe.get(v-1).iterator(); 
         while (it.hasNext()) { 
             i = it.next(); 
-            if (!visited[i]) 
-                topologicalSortUtil(i, visited, stack); 
+            if (!nodeVisited[i]) 
+                topSortU(i, nodeVisited, stack); 
         } 
-  
-        // Push current vertex to stack 
-        // which stores result 
+
+        // Füge aktuellen Knoten dem Stack hinzu
         stack.push(new Integer(v)); 
     } 
   
-    // The function to do Topological Sort. 
-    // It uses recursive topologicalSortUtil() 
-    void topologicalSort() 
+    // TopSort Funktion, die topSortU() benutzt
+    void topSort() 
     { 
         Stack<Integer> stack = new Stack<Integer>(); 
   
-        // Mark all the vertices as not visited 
-        boolean visited[] = new boolean[vertCount]; 
-        for (int i = 0; i < vertCount; i++) 
+        // Markiert alle Knoten als nicht überprüft
+        boolean visited[] = new boolean[nodeCount]; 
+        for (int i = 0; i < nodeCount; i++) 
             visited[i] = false; 
   
-        // Call the recursive helper 
-        // function to store 
-        // Topological Sort starting 
-        // from all vertices one by one 
-        for (int i = 1; i < vertCount; i++) 
+        // Starte topSort über topSortU
+         // Durchläuft alle Knoten nacheinander 1,2,3,4,5
+        for (int i = 1; i < nodeCount; i++) 
             if (visited[i] == false) 
-                topologicalSortUtil(i, visited, stack); 
+                topSortU(i, visited, stack); 
   
-        // Print contents of stack 
+        // Ausgabe
         while (stack.empty() == false) 
             System.out.print(stack.pop() + " "); 
     } 
